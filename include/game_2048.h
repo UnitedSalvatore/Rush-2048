7a9a99aa6bc449860ff727ba3741e73df7ae49cc
Yaroslav <salvaincorp@gmail.com>
2018-07-22 17:52:17 +0300

    mend

--- include/game_2048.h
+++ include/game_2048.h
@@ -6,7 +6,7 @@
 /*   By: ypikul <ypikul@student.42.unit.ua>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/21 14:14:36 by ypikul            #+#    #+#             */
-/*   Updated: 2018/07/22 17:47:02 by ypikul           ###   ########.fr       */
+/*   Updated: 2018/07/22 17:52:10 by ypikul           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ typedef enum e_const	t_winv;
 
 typedef enum	e_const
 {
+	NONE = 0u;
 	WIN_VALUE = 2048u
 }				t_const;
 
